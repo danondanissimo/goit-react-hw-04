6d942885c18c79e35f8c38a622065b4a5a596b7d
danondanissimo <106009630+danondanissimo@users.noreply.github.com>
2024-03-30 23:33:43 +0000

    Created functions and layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,58 +2,89 @@ import "./App.css";
 
 import { useEffect, useState } from "react";
 
-import ContactList from "./components/ContactList/ContactList.jsx";
+import ImageGallery from "./ImageGallery/ImageGallery.jsx";
+import { requestImagesByQuery } from "./components/Services/Api.js";
+import Loader from "./components/Loader/Loader.jsx";
+import ErrorMessage from "./components/ErrorMessage/ErrorMessage.jsx";
 
-import initialContacts from "./components/ContactList/contacts.json";
-import { nanoid } from "nanoid";
-import ContactForm from "./components/ContactForm/ContactForm";
+import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn.jsx";
+import toast, { Toaster } from "react-hot-toast";
 import SearchBox from "./components/SearchBox/SearchBox.jsx";
+import ImageModal from "./components/ImageModal/ImageModal.jsx";
 
 function App() {
-  const [contacts, setContacts] = useState(() => {
-    const stringifiedContacts = localStorage.getItem("contacts");
-    if (!stringifiedContacts) return initialContacts;
+  const [images, setImages] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [query, setQuery] = useState("");
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const [modalContent, setModalContent] = useState([]);
+  const notify = () => toast.error("Please fill in the Search field.");
 
-    const parsedContacts = JSON.parse(stringifiedContacts);
-    return parsedContacts;
-  });
+  const onSetSearchQuery = (searchTerm) => {
+    if (searchTerm.length === 0) {
+      notify();
+    } else {
+      setPage(1);
+      setQuery(searchTerm);
+    }
+  };
 
   useEffect(() => {
-    localStorage.setItem("contacts", JSON.stringify(contacts));
-  }, [contacts]);
+    if (query.length === 0) return;
 
-  const addContact = (formData) => {
-    const finalContact = {
-      ...formData,
-      id: nanoid(),
-    };
+    async function fetchImagesByQuery() {
+      try {
+        setLoading(true);
+        const response = await requestImagesByQuery(query, page);
 
-    setContacts((prevState) => {
-      return [...prevState, finalContact];
-    });
+        if (page !== 1) {
+          setImages((prevResult) => {
+            return [...prevResult, ...response.data.results];
+          });
+        } else {
+          setImages(response.data.results);
+        }
+
+        setTotalPages(response.data.total_pages);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchImagesByQuery();
+  }, [query, page]);
+
+  const loadMore = () => {
+    setPage((page) => page + 1);
+    window.scrollBy(0, 1000);
   };
 
-  const deleteContact = (contactId) => {
-    setContacts((prevState) => {
-      return prevState.filter((contact) => contact.id !== contactId);
-    });
+  const modalToggle = (likes, description, imgModal) => {
+    setIsOpen(!isOpen);
+    setModalContent([likes, description, imgModal]);
   };
 
-  const [filter, setFilter] = useState("");
+  console.log(modalContent);
 
-  const onChangeFilter = (event) => {
-    setFilter(event.target.value);
-  };
-
-  const filteredContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
   return (
     <>
-      <h1>Phonebook</h1>
-      <ContactForm addContact={addContact} />
-      <SearchBox value={filter} onChange={onChangeFilter} />
-      <ContactList contacts={filteredContacts} deleteContact={deleteContact} />
+      <SearchBox onSubmit={onSetSearchQuery} />
+      <Toaster />
+      {error && <ErrorMessage />}
+      {loading === true && <Loader />}
+      {images && <ImageGallery images={images} modalToggle={modalToggle} />}
+      {totalPages && page < totalPages && <LoadMoreBtn onClick={loadMore} />}
+      <ImageModal
+        isOpen={isOpen}
+        modalContent={modalContent}
+        modalToggle={modalToggle}
+        ariaHideApp={false}
+      />
     </>
   );
 }
